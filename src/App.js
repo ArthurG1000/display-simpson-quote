ca4e94cb61afdc0150d1858ef93373de0108d2ae
ArthurG1000 <garnierarthur.contact@gmail.com>
2021-10-26 11:43:30 +0200

    added finished Simpson challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import DisplaySimpson from './components/DisplaySimpson';
+import axios from 'axios';
+import { useState } from 'react';
+
+const sampleSimpson = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
+
 
 function App() {
+  const [simpson, setSimpson] = useState(" ");
+  const getSimpson = () => {
+    
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      
+      .then((response) => response.data)
+      
+      .then((data) => {
+        setSimpson(data[0]);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <DisplaySimpson simpson={simpson} />
+      <button type="button" onClick={getSimpson}>Get Simpson</button>
     </div>
   );
 }
